ba611c6dae023a48939026892cacdca52569cceb
Leo McClellan <mcclellan2@usf.edu>
2024-09-20 16:23:47 -0700

    Initialize employees with shift schedules

---
+++ employee_shift_management.js
@@ -0,0 +1,11 @@
+// Coding Challenge: Employee Shift Management System
+
+
+// Task 1:  Create an employee array of employee objects
+let employees = [
+    {name : "Andrew", shifts : [{day : "Monday", hours : 5}, {day : "Wednesday", hours : 4},{day : "Friday", hours : 10}]},
+    {name : "Bailey", shifts : [{day : "Tuesday", hours : 8}, {day : "Thursday", hours : 8}]},
+    {name : "Carly", shifts : [{day : "Saturday", hours : 10}]},
+    {name : "Denae", shifts : [{day : "Monday", hours : 3}, {day : "Wednesday", hours : 4},{day : "Sunday", hours : 10}]}
+];
+
