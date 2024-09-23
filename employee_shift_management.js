1e68980447b9eeea4de703db9959d2f61a512a27
Leo McClellan <mcclellan2@usf.edu>
2024-09-22 20:18:57 -0700

    Create listAvailableEmployees function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,6 +1,4 @@
 // Coding Challenge: Employee Shift Management System
-
-
 // Task 1:  Create an employee array of employee objects
 let employees = [
     {name : "Andrew", shifts : [{day : "Monday", hours : 5}, {day : "Wednesday", hours : 4},{day : "Friday", hours : 10}]},
@@ -15,7 +13,7 @@ function displayEmployeeShifts (employee) {
     console.log(`Employee name: ${employee.name}`);
     employee.shifts.forEach(shift =>
         console.log(`Shift on ${shift.day}: ${shift.hours}`)
-    )
+    );
 };
 displayEmployeeShifts(employees[0]);
 
@@ -26,20 +24,19 @@ function assignShift (aName, aDay, aHours) {
         // find employee and see if they have a shift for that day
         let shiftExists = findEmployee.shifts.find(shift => shift.day === aDay);
         if (shiftExists) {
-            return `Error. Shift already scheduled for ${aName} on ${aDay}`
+            return `Error. Shift already scheduled for ${aName} on ${aDay}`;
         }
         else {
             findEmployee.shifts.push({day : aDay, hours : aHours});
-            return `Shift assigned to ${aName}. ${aDay} for ${aHours} hours.`
+            return `Shift assigned to ${aName}. ${aDay} for ${aHours} hours.`;
         };
     }
     else {
-        return `Employee not found.`
+        return `Employee not found.`;
     };
 };
 // call function
 console.log(assignShift("Carly","Sunday",3));
-console.log(employees);
 
 // Task 4: Create a function to calculate total hours worked
 function calculateTotalHours (eName) {
@@ -59,3 +56,14 @@ function calculateTotalHours (eName) {
 };
 // call function
 console.log(calculateTotalHours("Andrew"));
+
+// Task 5: Create a function to list employees with free days
+function listAvailableEmployees(day) {
+    let availableEmployees = employees.shifts.filter(shift => shift.day != day);
+    if (availableEmployees.length >= 1) {   
+        availableEmployees.forEach(name => console.log(`Employee available for ${day}: ${name}`));
+    }
+    else {
+        console.log(`No available employees for ${day}.`);
+    };
+};
